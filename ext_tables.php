63a49342af70a2bbfc3bad6d5829acbd9bc9b1e4
Michael Knoll <mimi@kaktusteam.de>
2013-04-18 21:13:11 +0200

    TASK: Initial commit

---
+++ ext_tables.php
@@ -0,0 +1,5 @@
+<?php
+if (!defined ('TYPO3_MODE')) die ('Access denied.');
+
+// Register static TypoScript template for this extension
+t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript', '[davbb_sv] DAV Baden-Baden Kursverwaltung');
